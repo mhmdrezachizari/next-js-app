cb30914526eccc4b9f3807255d9e2140026f47c3
mohammadrezachizari <mhmdrezachizari@gmail.com>
2024-03-24 20:00:23 +0330

    salam

---
+++ components/ProductNavbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from './rang.module.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
+const ProductNavbar = () => {
+  return (
+    <>
+ <nav class="navbar navbar-expand-lg bg-primary" className={styles.colornav}>
+  <div class="container-fluid">
+    <Link class="navbar-brand" href="">Navbar</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" href="/home">Home</Link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" href="/home">Pricing</Link>
+        </li>
+     
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  );
+};
+
+export default ProductNavbar;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import ProductNavbar from "@/components/ProductNavbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+    <ProductNavbar/>
     <h1 style={{color:"red", alignItems:"center"}}>
       salam man mhmdreachizari hastam
     </h1>
